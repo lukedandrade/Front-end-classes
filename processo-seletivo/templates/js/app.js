1a6ca0546f4504592a8697e60c74aa28cb02bdc6
lukehell <spirolucas@hotmail.com>
2018-05-28 23:24:12 -0300

    Limpeza de código
    Remoção de código antigo

--- processo-seletivo/templates/js/app.js
+++ processo-seletivo/templates/js/app.js
@@ -1,32 +1,5 @@
 //Espera o carregamento do documento.
 $(document).ready(function (){
-	var $btnConhecaMe = $('#conheca-me');
-	$btnConhecaMe.on('click', function(){
-		var conf = confirm('Você deseja continuar ?');
-		if (conf){
-			alert('Muito obrigado!');
-		} else{
-			alert(':(');
-		}
-		console.log(conf);
-	});
-
-	$('.scroll-animate').on('click', function (event) {
-		var $element, options, duration;
-		//evita que a ação padrão do botão seja chamada.
-		event.preventDefault();
-		//pega o elemento clicado
-		$element = $($.attr(this, 'href'));
-		//opções da animação
-		options = {
-			scrollTop: $element.offset().top
-		};
-		//duração em ms
-		duration = 500;
-		//adiciona o efeito de animação
-		$('html, body').animate(options, duration);
-	});
-
 	var $btnToogle = $('#toogle');
 	$btnToogle.on('click', function(){
 		var formulario = document.getElementById('formulario');
